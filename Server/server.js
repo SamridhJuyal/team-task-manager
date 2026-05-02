f97024459fe2934162518c3653def01cca51bb2d
Samridh <samridh904@gmail.com>
2026-05-02 13:58:37 +0530

    final fix cors

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,18 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -18,11 +30,8 @@ app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 
 const PORT = process.env.PORT;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
