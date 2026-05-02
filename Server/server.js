4eee675a6e2e11635e3e6901e9256a09821875a4
Samridh <samridh904@gmail.com>
2026-05-02 12:29:42 +0530

    fix railway port issue

--- Server/server.js
+++ Server/server.js
@@ -5,20 +5,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working" });
 });
 
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -26,7 +20,7 @@ app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
   .connect(process.env.MONGO_URI)
